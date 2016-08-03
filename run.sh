10969776d739bfa778c3d1c5674c3f318693a3a4
Simba Zhang <solderzzc@gmail.com>
2016-08-03 09:41:42 -0700

    Fixed runtime script.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --name my-running-script -v "$PWD/export":/usr/src/myapp/export  account-creater ruby account_creator.rb
+docker run -it --rm --name account-creator-script -v "$PWD/export":/usr/src/app/export  account-creater ruby account_creator.rb
